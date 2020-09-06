1fb5fbd98ec397d8eeaabc393c045093e1efa9e8
Felipe Miranda <lipemiranda159@hotmail.com>
2020-09-05 21:59:47 -0300

    Improve cliente controller documentation

--- Application/Controllers/ClienteController.cs
+++ Application/Controllers/ClienteController.cs
@@ -1,6 +1,7 @@
 ﻿using devboost.dronedelivery.felipe.DTO.Models;
 using devboost.dronedelivery.felipe.EF.Repositories.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,36 +9,54 @@ using System.Threading.Tasks;
 
 namespace devboost.dronedelivery.felipe.Controllers
 {
+    /// <summary>
+    /// Controller com ações referentes aos clientes
+    /// </summary>
     //[Authorize("Bearer")]
     [Route("api/[controller]")]
     [ApiController]
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public class ClienteController
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     {
         private readonly IClienteRepository _clienteRepository;
-
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="clienteRepository"></param>
         public ClienteController(IClienteRepository clienteRepository)
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
-
         {
             _clienteRepository = clienteRepository;
         }
-
+        /// <summary>
+        /// Retorna lista de clientes cadastrados no banco
+        /// </summary>
+        /// <returns>Uma lista de clientes</returns>
         [HttpGet]
-        [AllowAnonymous]
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        [ProducesResponseType(typeof(IEnumerable<Cliente>), StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<Cliente>> Get()
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             return _clienteRepository.GetClientes().ToList();
         }
-
+        /// <summary>
+        /// Cria um novo cliente
+        /// </summary>
+        /// <param name="cliente">Modelo cliente</param>
+        /// <remarks>
+        ///Exemplo:
+        ///
+        ///     POST /api/cliente
+        ///     {
+        ///      "nome": "Joao",
+        ///      "latitude": -23.5631043,
+        ///      "longitude": -46.6565712,
+        ///      "userId": "joao",
+        ///      "password": "1234"
+        ///     }
+        ///
+        /// </remarks>        
+        /// <returns>O novo cliente</returns>
+        [ProducesResponseType(typeof(Cliente), StatusCodes.Status200OK)]
         [HttpPost]
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public async Task<ActionResult<Cliente>> Post(Cliente cliente)
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
         {
             await _clienteRepository.SaveCliente(cliente);
             return cliente;
