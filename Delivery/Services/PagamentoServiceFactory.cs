727a20119a3d2384b2ff44818602331ff43ddbd0
Felipe Miranda <lipemiranda159@hotmail.com>
2020-09-07 01:45:51 -0300

    fix references

---
+++ Delivery/Services/PagamentoServiceFactory.cs
@@ -0,0 +1,25 @@
+﻿using devboost.dronedelivery.domain.core;
+using devboost.dronedelivery.domain.core.Enums;
+using devboost.dronedelivery.domain.Interfaces;
+using System;
+
+namespace devboost.dronedelivery.felipe.Services
+{
+    public class PagamentoServiceFactory : IPagamentoServiceFactory
+    {
+        private readonly PaymentSettings _paymentSettings;
+        public PagamentoServiceFactory(PaymentSettings payment)
+        {
+            _paymentSettings = payment;
+        }
+        public IPagamentoServico GetPagamentoServico(ETipoPagamento tipoPagamento)
+        {
+            switch (tipoPagamento)
+            {
+                case ETipoPagamento.CARTAO:
+                    return new PagamentoCartaoServico(_paymentSettings);
+                default: throw new NotImplementedException("Servico não implementado!");
+            }
+        }
+    }
+}
